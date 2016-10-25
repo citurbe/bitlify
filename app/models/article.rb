62dbd184f520d2259d8c6d42c9decb84eaee41af
Craig Iturbe <citurbe@gmail.com>
2016-10-25 09:51:12 -0400

    automatically create bitly links

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,9 +4,10 @@ class Article < ApplicationRecord
   def self.create(params)
     @article = Article.new
     new_link = params[:link] + "?login=8j7k9h0l&password=gu$ryd@wnw0w"
+    newer_link = Bitly.client.shorten(new_link)
     title = Mechanize.new.get("#{new_link}").title
     @article.title = title
-    @article.link = new_link
+    @article.link = newer_link.short_url
     @article.save
     @article
   end
---
+++ config/initializers/bitly.rb
@@ -0,0 +1,6 @@
+Bitly.use_api_version_3
+
+Bitly.configure do |config|
+  config.api_version = 3
+  config.access_token = "a09223e5d1039dfdd35bdbb26c4aadf887145cf0"
+end
